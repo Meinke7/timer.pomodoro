c7ee805d710dd667bb4c3a0aad08a66f765558d4
Meinke7 <carlos.meinke7@gmail.com>
2023-08-23 15:17:48 -0300

    add countdown

--- src/FocusTimer/timer.js
+++ src/FocusTimer/timer.js
@@ -1,6 +1,19 @@
 import state from './state.js'
+import * as el from './elements.js'
+
+export function countdown() {
+  if(!state.isRunning) {
+    return
+  }
+
+  setTimeout(() =>  countdown(), 1000)
+  
+}
 
 export function updateDisplay(minutes, seconds) {
   minutes = minutes ?? state.minutes
   seconds = seconds ?? state.seconds
+
+  el.minutes.textContent = String(minutes).padStart(2, "0")
+  el.seconds.textContent = String(seconds).padStart(2, "0")
 }
